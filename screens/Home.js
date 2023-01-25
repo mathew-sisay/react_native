aeecf223949131f72beeeffc0604de9b431e5745
mathew-sisay <mathewsisay835@gmail.com>
2023-01-25 12:29:01 +0000

    first api test

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
-import { View, SafeAreaView, FlatList } from "react-native";
+import React, { useState,useEffect  } from "react";
+import { View, SafeAreaView, FlatList,Text,styles } from "react-native";
 
 import { NFTCard, HomeHeader, FocusedStatusBar } from "../componenets";
 import { COLORS, NFTData } from "../constants";
+import axios from 'axios'
 
 const Home = () => {
   const [nftData, setNftData] = useState(NFTData);
+  
+   const [data,setData] = useState([])
+  
+  useEffect (() => {
 
+    async function fecthData() {
+
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
+      setData(data )
+    }
+      fecthData()
+  },[])
+
+       
   const handleSearch = (value) => {
     if (value.length === 0) {
       setNftData(NFTData);
@@ -24,7 +38,17 @@ const Home = () => {
   };
 
   return (
+
+    
     <SafeAreaView style={{ flex: 1 }}>
+
+
+        <View>
+          <Text >{data.title}</Text>
+          <Text>{data.body}</Text>
+        </View>
+  
+
       <FocusedStatusBar backgroundColor={COLORS.primary} />
       <View style={{ flex: 1 }}>
         <View style={{ zIndex: 0 }}>
